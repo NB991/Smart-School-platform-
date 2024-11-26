55630decc80356a8d16df2b4f10743f5211b1381
Omar Maher <leonardo.hitoska@gmail.com>
2024-11-26 15:13:25 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -3,45 +3,51 @@ const teacherTable = document.getElementById('teacherTable');
 
 
 function loadTeachers() {
-  const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
-  teacherTable.innerHTML = '';
+    const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
+    teacherTable.innerHTML = '';
 
-  teachers.forEach((teacher, index) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${teacher.name}</td>
-      <td>${teacher.nationalId}</td>
-      <td>${teacher.quota}</td>
-      <td>${teacher.delegation}</td>
-      <td><button onclick="deleteTeacher(${index})">حذف</button></td>
-    `;
-    teacherTable.appendChild(row);
-  });
+    teachers.forEach((teacher, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${teacher.name}</td>
+        <td>${teacher.nationalId}</td>
+        <td>${teacher.quota}</td>
+        <td>${teacher.realquota}</td>
+        <td>${teacher.delegation}</td>
+        <td>${teacher.date}</td>
+        <td><button onclick="deleteTeacher(${index})">حذف</button></td>
+        `;
+        teacherTable.appendChild(row);
+    });
 }
 
 
 function addTeacher(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  const name = document.getElementById('name').value;
-  const nationalId = document.getElementById('nationalId').value;
-  const quota = document.getElementById('quota').value;
-  const delegation = document.getElementById('delegation').value;
+    const name = document.getElementById('name').value;
+    const nationalId = document.getElementById('nationalId').value;
+    const quota = document.getElementById('quota').value;
+    const name = document.getElementById('realquota').value;
+    const delegation = document.getElementById('delegation').value;
+    const name = document.getElementById('date').value;
 
-  const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
-  teachers.push({ name, nationalId, quota, delegation });
-  localStorage.setItem('teachers', JSON.stringify(teachers));
+    const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
+    teachers.push({
+        name, nationalId, quota, delegation
+    });
+    localStorage.setItem('teachers', JSON.stringify(teachers));
 
-  form.reset();
-  loadTeachers();
+    form.reset();
+    loadTeachers();
 }
 
 
 function deleteTeacher(index) {
-  const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
-  teachers.splice(index, 1);
-  localStorage.setItem('teachers', JSON.stringify(teachers));
-  loadTeachers();
+    const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
+    teachers.splice(index, 1);
+    localStorage.setItem('teachers', JSON.stringify(teachers));
+    loadTeachers();
 }
 
 
