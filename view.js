f9e8feca5dd39de4b7e3444b931ee450d104e035
Omar Maher <leonardo.hitoska@gmail.com>
2024-11-26 15:14:51 +0200

    Update view.js

--- view.js
+++ view.js
@@ -1,18 +1,20 @@
 function loadTeachers() {
-  const teacherTable = document.getElementById('teacherTable');
-  const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
-  teacherTable.innerHTML = '';
+    const teacherTable = document.getElementById('teacherTable');
+    const teachers = JSON.parse(localStorage.getItem('teachers')) || [];
+    teacherTable.innerHTML = '';
 
-  teachers.forEach((teacher) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${teacher.name}</td>
-      <td>${teacher.nationalId}</td>
-      <td>${teacher.quota}</td>
-      <td>${teacher.delegation}</td>
-    `;
-    teacherTable.appendChild(row);
-  });
+    teachers.forEach((teacher) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${teacher.name}</td>
+        <td>${teacher.nationalId}</td>
+        <td>${teacher.quota}</td>
+        <td>${teacher.realquota}</td>
+        <td>${teacher.delegation}</td>
+        <td>${teacher.date}</td>
+        `;
+        teacherTable.appendChild(row);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', loadTeachers);
